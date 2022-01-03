7b713783b2f3ee9658938c3193c855d760e42b8c
hoonzinope <hoonzinope@gmail.com>
2022-01-03 22:42:29 +0900

    회원 등록, 수정, 조회 API 구현

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -39,4 +39,10 @@ public class MemberService {
     public Member findOne(Long memberID){
         return memberRepository.findOne(memberID);
     }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name); // 변경 감지!!
+    }
 }
