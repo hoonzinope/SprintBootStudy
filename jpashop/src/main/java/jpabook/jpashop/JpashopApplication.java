01a3348e611d09c3ef8865db795970df4a2dd837
hoonzinope <hoonzinope@gmail.com>
2022-01-09 22:17:18 +0900

    API 만들기 (order 불러오기 v1 / 엔티티 직접 조회시 발생되는 문제점 학습)

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -10,4 +12,13 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		//return new Hibernate5Module(); // 지연로딩의 경우, API로 엔티티 호출시 LAZY LOADING일때 외부노출을 막는다 (무한 루프 방지)
+
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+		//강제 지연 로딩 설정 (지연로딩 인 column도 가져오게끔!)
+//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING,true); -> 쓰지 않는게 좋음!
+		return hibernate5Module;
+	}
 }
---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/*
+* X to One 관계 (ManyToOne, OneToOne)
+* Order
+* Order -> Member
+* Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    } // 양방향 연관관계 때문에 계속 불러오는 문제 발생!
+}
